309401da5bb8e0893a9f14e0ee5cbfbf8a6907c8
AkanMNdem <123981950+AkanMNdemnoreply.github.com>
2024-01-13 12:00:12 -0500

    feat(task-completion)
    - Implement handling task completion to index file

--- task_manager/pages/index.js
+++ task_manager/pages/index.js
@@ -1,6 +1,6 @@
 import Task from './task'
 import { Raleway } from 'next/font/google'
-
+import {getAllTodos, updateTask, addTask, updateTaskList, deleteTask} from './api/hello'
 const raleway = Raleway({
   weight: '400',
   subsets: ['latin'],
@@ -11,6 +11,21 @@ export default function Home() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState('');
   const [percentageCompleted, setPercentageCompleted] = useState(0);
+
+  const handleTaskCompletion = (id) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, status: !task.status };
+        } else {
+          return task;
+        }
+      });
+      updateTaskList(updatedTasks);
+      return updatedTasks;
+    });
+  }
+
 
   return (
     <div className={raleway.className}>
